e8772ab2f0ced9c349d8fa7b77000890fa875828
ArieldRio <arieldelrioviamonte@gmail.com>
2020-07-24 19:40:10 -0500

    Updates

---
+++ precache-manifest.4d56f3948431d5820ea250eb01473f7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e1ecf69e7b8421799401e7a83210de7",
+    "url": "/personal-portfolio/index.html"
+  },
+  {
+    "revision": "e6b2e86150b2c3abcd8f",
+    "url": "/personal-portfolio/static/css/main.9b38bcb3.chunk.css"
+  },
+  {
+    "revision": "c7847991605a24b57966",
+    "url": "/personal-portfolio/static/js/2.688e4376.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/personal-portfolio/static/js/2.688e4376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6b2e86150b2c3abcd8f",
+    "url": "/personal-portfolio/static/js/main.61530e43.chunk.js"
+  },
+  {
+    "revision": "fbd52b79a2256129d0f1",
+    "url": "/personal-portfolio/static/js/runtime-main.225001ee.js"
+  }
+]);
